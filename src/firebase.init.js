559bf6a1dcec28b37bfce263e54820d44c6b8d57
aklimaakhter <aklimawebd@gmail.com>
2022-05-08 05:15:20 +0600

    env variable set up of firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD7ociI_p9DeH80fP6gB9ggd2IkENt0_wc",
-    authDomain: "book-warehouse-management.firebaseapp.com",
-    projectId: "book-warehouse-management",
-    storageBucket: "book-warehouse-management.appspot.com",
-    messagingSenderId: "1079212251079",
-    appId: "1:1079212251079:web:63f6ae5480103f955cabe2"
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket:process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
