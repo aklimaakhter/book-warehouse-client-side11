3f9f90a0a6d200aab8c3e4c162841cf4046571d4
aklimaakhter <aklimawebd@gmail.com>
2022-05-10 13:34:38 +0600

    banner changed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 
-// import banner1 from '../../../images/banner/banner-1.png';
+
 import banner2 from '../../../images/banner/banner-2.png';
-// import banner3 from '../../../images/banner/banner-3.png';
+import banner3 from '../../../images/banner/banner-3.jpg';
+
 
 const Banner = () => {
     const [index, setIndex] = useState(0);
@@ -13,17 +14,6 @@ const Banner = () => {
     };
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
-            {/* <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={banner1}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3></h3>
-                    <p></p>
-                </Carousel.Caption>
-            </Carousel.Item> */}
             <Carousel.Item>
                 <img
                     className="d-block w-100"
@@ -36,7 +26,7 @@ const Banner = () => {
                     <p></p>
                 </Carousel.Caption>
             </Carousel.Item>
-            {/* <Carousel.Item>
+            <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src={banner3}
@@ -45,7 +35,7 @@ const Banner = () => {
 
                 <Carousel.Caption>
                 </Carousel.Caption>
-            </Carousel.Item> */}
+            </Carousel.Item>
         </Carousel>
     );
 };
