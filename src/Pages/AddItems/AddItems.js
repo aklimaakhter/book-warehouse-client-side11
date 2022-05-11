b3094470f0b135458135696fe8e4d0e61ecf34e5
aklimaakhter <aklimawebd@gmail.com>
2022-05-11 18:45:17 +0600

    add item

--- src/Pages/AddItems/AddItems.js
+++ src/Pages/AddItems/AddItems.js
@@ -8,10 +8,11 @@ const AddItems = () => {
         <div className='w-50 mx-auto'>
             <h2>Please add Items</h2>
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("name", { required: true, maxLength: 20 })} />
-                <input {...register("description", { pattern: /^[A-Za-z]+$/i })} />
-                <input type="number" {...register("age", { min: 18, max: 99 })} />
-                <input type="submit" />
+                <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                <textarea className='mb-2' placeholder='Description' {...register("description")} />
+                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+                <input className='mb-2' placeholder='Photo URL' type="tet" {...register("img")} />
+                <input type="submit" value='Add Item' />
             </form>
         </div>
     );
